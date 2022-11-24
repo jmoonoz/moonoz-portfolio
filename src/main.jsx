4cb472655a5d5089e2d5527e21e5457bbea83795
Joel Munoz <j.munoz1@yahoo.com>
2022-11-24 14:47:09 -0800

    files created, added to folders

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import GlobalStyles from './style/GlobalStyle'
+import Typography from './style/Typography'
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
     <GlobalStyles />
+    <Typography />
     <App />
   </>
 )
